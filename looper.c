be229ba810d5058a177ce8d55324453174cb21a7
Shaji Khan <quotablecode@gmail.com>
2024-02-18 22:08:24 +0530

    looper

--- looper.c
+++ looper.c
@@ -21,11 +21,114 @@
 
 #define URI "http://shaji.in/plugins/eg-amp"
 
+/** Define a macro for converting a gain in dB to a coefficient. */
+#define DB_CO(g) ((g) > -90.0f ? powf(10.0f, (g) * 0.05f) : 0.0f)
+
 typedef enum {
 	TOGGLE_RECORD   = 0,
-    TOGGLE_PLAY = 1,
-    TOGGLE_FILE = 2,
-	INPUT  = 3,
-	OUTPUT = 4
+	TOGGLE_PLAY = 1,
+	TOGGLE_FILE = 2,
+	GAIN = 3,
+	BUFFER_SIZE = 4,
+    START = 5,
+    END = 6,
+	INPUT  = 7,
+	OUTPUT = 8
 } PortIndex;
 
+typedef struct {
+	// Port buffers
+	float * gain;
+    float * toggle_play ;
+    float * toggle_rec ;
+	float * input;
+	float * output;
+	float * buffer_size ;
+	float * buffer ;
+    float * start ;
+    float * end ;
+    int counter ;
+} Looper;
+
+static LV2_Handle
+instantiate(const LV2_Descriptor*     descriptor,
+            double                    rate,
+            const char*               bundle_path,
+            const LV2_Feature* const* features)
+{
+	Looper * looper = (Looper*)malloc(sizeof(Looper));
+	return (LV2_Handle)looper;
+}
+
+static void
+connect_port(LV2_Handle instance,
+             uint32_t   port,
+             void*      data)
+{
+	Looper* looper = (Looper*)instance;
+
+	switch ((PortIndex)port) {
+	case GAIN:
+		looper->gain = (const float*)data;
+		break;
+	case INPUT:
+		looper->input = (const float*)data;
+		break;
+	case OUTPUT:
+		looper->output = (float*)data;
+		break;
+    case TOGGLE_PLAY:
+        looper -> toggle_play = (float *) data ;
+        break ;
+    case TOGGLE_RECORD:
+        looper -> toggle_rec = (float *) data ;
+        break ;
+    case BUFFER_SIZE:
+        looper -> buffer_size = (float *) data ;
+        break ;
+    case START:
+        looper -> start = (float *) data ;
+        break ;
+    case END:
+        looper -> end = (float *) data ;
+        break ;
+	}
+}
+
+static void
+activate(LV2_Handle instance) {
+    Looper * looper = (Looper *) instance ;
+    * looper -> start = 0 ;
+    * looper -> end = 0 ;
+    looper -> counter = 0 ;
+    looper -> buffer = malloc (* looper -> buffer_size * 1024) ;
+}
+
+static void
+run(LV2_Handle instance, uint32_t n_samples)
+{
+	const Looper * looper = (const Looper *)instance;
+
+	const float        gain   = *(looper->gain);
+	const float* const input  = looper->input;
+	float* const       output = looper->output;
+
+    if (* looper -> toggle_rec > 0) {
+        for (uint32_t pos = 0; pos < n_samples; pos++) {
+            buffer [looper -> counter] = input [pos] ;
+            looper -> counter ++ ;
+            if (looper -> counter > * looper -> buffer_size) {
+                * looper -> toggle_rec = 0 ;
+                looper -> counter = 0 ;
+                break ;
+            }
+        }
+
+    }
+
+	const float coef = DB_CO(gain);
+
+	for (uint32_t pos = 0; pos < n_samples; pos++) {
+		output[pos] = input[pos] * coef;
+	}
+}
